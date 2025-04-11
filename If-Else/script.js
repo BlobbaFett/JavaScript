94a2ce8aea84f7df6b0b2746fc8c3a34a4e3b2a6
BlobbaFett <quebecmc100@gmail.com>
2025-04-11 13:37:16 -0600

    tareas 11 abril

---
+++ If-Else/script.js
@@ -0,0 +1,37 @@
+let hour = prompt('Enter an hour (0-23):')
+
+if (hour < 12){
+    alert('Good morning!');
+} 
+else if (hour < 18){
+    alert('Good afernoon!');
+}
+else{
+    alert('Good evening!')
+}
+
+let number = prompt('Enter a number')
+if(number % 2 == 0){
+    alert('Even Number');
+}
+else{
+    alert('Odd Number')
+}
+
+let score = prompt('Enter the Scholarship')
+if(score > 85){
+    alert('Eligible for scholarship')
+}
+else{
+    alert('Not eligible')
+}
+
+
+let coin = prompt('Heads or Tails')
+let flip = ('Heads')
+if(coin =  ){
+    alert('heads')
+}
+else{
+    alert{}
+}
---
+++ Js.Functions/script.js
@@ -0,0 +1,23 @@
+function add7(num) {
+    return 4 + num
+}
+let sum = add7(7);
+console.log(sum);
+
+function multiply(num2){
+    return num2 * (4+2)
+}
+let product = multiply(2)
+console.log(product)
+
+function lastletter(word){
+
+}
+
+console.log(word);
+
+function capitalize(word2){
+    return 
+}
+let capital = word2('juanito')
+console.log(capital);
