9636d231494760d93809edf28743fcb8632c261d
BlobbaFett <quebecmc100@gmail.com>
2025-04-30 09:39:52 -0600

    .

--- light bulb/script.js
+++ light bulb/script.js
@@ -1,12 +1,9 @@
-let light = document.querySelector('.light-box')
-let switch_light = document.querySelector('.switch-box');
-    function light_on(){
-    
-    if (light == '.light-on'){
-        light.classList.toggle("light-off")
-        switch_light.classList.toggle("switch-off");
-    } else {
-        light.classList.toggle("light-on")
-        switch_light.classList.toggle("switch-on");
-    }
+function light_on(){
+    let light = document.querySelector('.light-box')
+    console.log(light)
+    let switch_light = document.querySelector('.switch-box');
+    console.log(switch_light)
+    light.classList.toggle("light-on")
+    switch_light.classList.toggle("switch-on");
+
 }
