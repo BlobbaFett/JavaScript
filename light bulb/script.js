daed2a73e22908238c6be92ad81ff3e26dcbdaef
BlobbaFett <quebecmc100@gmail.com>
2025-04-30 09:32:53 -0600

    .

--- light bulb/script.js
+++ light bulb/script.js
@@ -1,3 +1,12 @@
-function changeColor(){
-    let light = document.querySelector('div')
+let light = document.querySelector('.light-box')
+let switch_light = document.querySelector('.switch-box');
+    function light_on(){
+    
+    if (light == '.light-on'){
+        light.classList.toggle("light-off")
+        switch_light.classList.toggle("switch-off");
+    } else {
+        light.classList.toggle("light-on")
+        switch_light.classList.toggle("switch-on");
+    }
 }
