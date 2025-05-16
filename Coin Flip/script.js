19f1205405a2d6aeda6630ce3f835e2f04c7d861
BlobbaFett <quebecmc100@gmail.com>
2025-05-16 11:32:26 -0600

    .

--- Coin Flip/script.js
+++ Coin Flip/script.js
@@ -1,9 +1,29 @@
-let heads = document.querySelector('heads')
-let tails = document.querySelector('tails')
+let Rcoin = document.querySelector('#coin');
 
-let Rcoin = document.querySelector('coin')
-
-function hort(){
-    
-    
+function HeaoTA(x, y){
+    let HoT = Math.random();;
+    if (HoT <= 0.5) {
+        return 'Ganaste Pequeño' + x;
+    }
+    else {
+        return 'Perdiste' + y;
+    }
 }
+function HeaoTA2(x, y){
+    let HoT = Math.random();;
+    if (HoT <= 0.5) {
+        return 'Ganaste Pequeño' + y;
+    }
+    else {
+        return 'Perdiste' + x;
+    }
+}
+heads.addEventListener('click', function() {
+    HeaoTA('🟡', '🪙')
+    Rcoin.textContent = HeaoTA('🟡', '🪙')
+});
+
+tails.addEventListener('click', function(){
+    HeaoTA2('🟡', '🪙')
+    Rcoin.textContent = HeaoTA2('🟡', '🪙')
+})
